ae4c76e1ec16eea7d7ec9264fe0e54599b393b70
uwroute <liuxun001@126.com>
2015-09-05 21:09:46 +0800

    add code

--- ML/parallel_lr/read.h
+++ ML/parallel_lr/read.h
@@ -12,6 +12,19 @@
 #ifndef _READ_H_
 #define _READ_H_
 
+#include "data.h"
+#include "Common/thread.h"
+#include "Common/lock.h"
+#include "Common/msg_queue.h"
 
+namespace ML{
 
+class ReadThread : public Common::Thread {
+public:
+	
+private:
+
+};
+
+}
 #endif  // _READ_H_
